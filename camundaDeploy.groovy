cfc427647562a214f421c00d7576cc6dfceb6d82
Stephen Russett <stephenrussett@gmail.com>
2017-06-05 22:42:03 -0400

    remove old groovy file

--- camundaDeploy.groovy
+++
@@ -1,8 +0,0 @@
-@Grab('org.apache.commons:commons-math3:3.4.1')
-import org.apache.commons.math3.primes.Primes
-void parallelize(int count) {
-  if (!Primes.isPrime(count)) {
-    error "${count} was not prime"
-  }
-  // …
-}
